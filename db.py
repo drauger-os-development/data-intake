47935a8b91a85be0558ccf7e9da3b8aaa4016a1e
Thomas Castleman <31315409+Batcastle@users.noreply.github.com>
2023-01-24 09:02:22 -0500

    Create db.py

---
+++ db.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import sqlite3 as sql
+
+
+# this is the database handler file. It handles input and output to the database in a multi-threaded, thread-safe
+# environment, through a simple API
